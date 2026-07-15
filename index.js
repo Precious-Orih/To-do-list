ddf59f4901eed01036fcf66f6f106b8a65004ae3
Ehi-Orih <ehiorih25@gmail.com>
2026-07-15 14:05:26 +0100

    adding delete button and date

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 let list = [];
+let todaysDate = new Date().toISOString().split('T')[0];
+
+
+
+
 displayTask();
-let input = document.getElementById('input');
-let add = document.getElementById('add').onclick = () => { addTask(); };
-let enterInput = document.getElementById('input').onkeydown = (event) => {
+let taskTitle = document.getElementById('taskTitle');
+let taskDate = document.getElementById('taskDate');
+
+const add = document.getElementById('add').onclick = () => { addTask(); };
+const enterInput = document.getElementById('taskTitle').onkeydown = (event) => {
     if (event.key === "Enter") { addTask(); }
 };
+
 function displayTask() {
     let taskList = '';
     for (let i = 0; i < list.length; i++){
-        taskList += `<li>${list[i]}</li>`;
+        taskList +=
+            `<li>
+                ${list[i].title},
+                ${list[i].date}
+                <button class="deleteBtn"  onclick = "deleteTask(${i});">Delete</button>
+            </li>`;
     };
-    document.getElementById('taskList').innerHTML = taskList;
+    document.getElementById('taskList').innerHTML = taskList;    
 }
 function addTask() {
-    list.push(input.value);
-    input.value = '';
+    list.push({ title: taskTitle.value, date: taskDate.value });
+    console.log(list);
+    displayTask();console.log(taskDate.value);
+    taskTitle.value = '';
+    taskDate.value = '';
+    
+}
+function deleteTask(i) {
+    list.splice(i, 1);
     console.log(list);
     displayTask();
 }
+
 
 
 
